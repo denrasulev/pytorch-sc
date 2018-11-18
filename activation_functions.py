df283a3c63a5bb566257d9b547120e350be18878
Denis Rasulev <ranklord@gmail.com>
2018-11-18 09:36:57 +0100

    Add code for Hyperbolic Tangent Activation function.

--- activation_functions.py
+++ activation_functions.py
@@ -1,4 +1,4 @@
-# Avoid in practice!
+# Sigmoid - avoid in practice!
 #
 # Sigmoids saturate and kill gradients
 # Sigmoids slow confergence
@@ -13,3 +13,12 @@ def sigmoid(x, derivative=False):
         return x * (1 - x)
     else:
         return 1 / (1 + np.exp(-x))
+
+
+# Hyperbolic Tangent Function
+
+def tanh(x, derivative=False):
+    if derivative is True:
+        return 1 - x ** 2
+    else:
+        return np.tanh(x)
