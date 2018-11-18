6855880100fbd4b18066bfbf59a77317f7f7a1f6
Denis Rasulev <ranklord@gmail.com>
2018-11-18 10:35:27 +0100

    Add code for ReLU - Rectified Linear Unit

--- activation_functions.py
+++ activation_functions.py
@@ -1,13 +1,7 @@
-# Sigmoid - avoid in practice!
-#
-# Sigmoids saturate and kill gradients
-# Sigmoids slow confergence
-# Sigmoids are not zero centered
-# OK to use on last layer
-
 import numpy as np
 
 
+# Sigmoid
 def sigmoid(x, derivative=False):
     if derivative is True:
         return x * (1 - x)
@@ -16,9 +10,32 @@ def sigmoid(x, derivative=False):
 
 
 # Hyperbolic Tangent Function
-
 def tanh(x, derivative=False):
     if derivative is True:
         return 1 - x ** 2
     else:
         return np.tanh(x)
+
+
+# ReLU - Rectified Linear Unit
+def ReLU(x):
+    return x * (x > 0)
+
+
+# Sigmoid - avoid in practice!
+# Sigmoids saturate and kill gradients
+# Sigmoids slow confergence
+# Sigmoids are not zero centered
+# OK to use on last layer
+
+# TanH - avoid in practice!
+
+# ReLU - Rectified Linear Unit
+# Learns faster
+# Avoids vanishing gradient
+# Only use for the hidden layers!
+# ReLU could result in dead neurons (then use Leaked ReLU)
+# Output - Use SoftMax for Multi Class
+# Output - Use Linear function for regression
+# More implementations:
+# https://stackoverflow.com/questions/32109319/how-to-implement-the-relu-function-in-numpy
